ccfbd4baaf0a934a5cc5dc5ba2bfba3d31ebb534
cabreira <thcabreira0@gmail.com>
2022-04-19 20:38:34 -0300

    dialetos adicionados

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 async function traduzirTexto() {
-    let texto = document.getElementById("textoPraTraduzir").value.toString();
+    let texto = document.getElementById("textoPraTraduzir").value.toString()
+    let dialeto = document.getElementById("dialetoForm")
+    console.log(texto)
 
     const response = await fetch("./alfabeto.json");
     const obj = await response.json();
-    const { alfabeto } = obj
+    const { leonxzv, cabreira } = obj
+    //const { alfabeto, simples } = obj
 
-    alfabeto.forEach((x) => {
-        texto = texto.replaceAll(x.chave, x.valor);
-    });
+    console.log(obj)
+    for (let i = 0; i < dialeto.length; i++) {
+        if (dialeto[i].checked) {
+            let dialetoUtilizado = dialeto[i].value.toString()
+            if (dialetoUtilizado == "leonxzv") {
+                leonxzv.forEach((x) => {
+                    texto = texto.replaceAll(x.chave, x.valor);
+                    document.getElementById("textoTraduzido").value = texto;
+                })
+            }
 
-    document.getElementById("textoTraduzido").value = texto;
+            if (dialetoUtilizado == "cabreira") {
+                cabreira.forEach((x) => {
+                    texto = texto.replaceAll(x.chave, x.valor);
+                    document.getElementById("textoTraduzido").value = texto;
+                })
+            }
+        }
+
+    }
+
+    // alfabeto.forEach((x) => {
+    //     texto = texto.replaceAll(x.chave, x.valor);
+    //     document.getElementById("textoTraduzido").value = texto;
+    // })
+
+
 }
